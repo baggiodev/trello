90ff59d0f448cd7526411d703b98b8f3e67d4c06
Baggio Shehadi <k3nh1n@yahoo.com>
2018-11-09 09:25:57 -0600

    boilerplate

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+// Enable CORS
+app.use(cors());
+
+// Request body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan("dev"));
+// Request body cookie parser
+app.use(cookieParser());
+
+if(process.env.NODE_ENV === "production"){
+	app.use(express.static("client/build"));
+}
+
+mongoose.connect("mongodb://localhost/test");
+mongoose.connection.once("open", () => {
+	console.log("conneted to database");
+});
+
+// bind express with graphql
+app.use("/graphql", graphqlHTTP({
+	schema,
+	graphiql: true
+}));
+
+app.get("*", function(req, res) {
+	res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT,function(){
+	console.log(`Serving on port ${PORT}`);
+});
