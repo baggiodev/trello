f83d1bd7f105470de8dbd35b5435add112a94800
Baggio Shehadi <k3nh1n@yahoo.com>
2018-12-22 11:09:35 -0600

    ref #1 need to add apollo-client still

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,22 @@
 import {render} from "react-dom";
 import React from "react";
 import {Provider} from "react-redux";
-
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import App from "./containers/App";
 import store from "./store";
+// components
+
+// apollo client setup
+const client = new ApolloClient({
+    uri: "http://localhost:4000/graphql"
+});
+
 
 render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+	<ApolloProvider client={client}>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</ApolloProvider>,
     window.document.getElementById("root"));
